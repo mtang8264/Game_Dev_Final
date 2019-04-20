15f14be8abaf767ca86cc67d0b8b0de12d9cb535
Michael Poltronieri Tang <MTang8264@gmail.com>
2019-04-20 17:34:15 -0400

    Added basic NPC functionality and the ability for them to stare at the player

---
+++ Assets/Scripts/NPCIdle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCIdle : MonoBehaviour
+{
+    private Animator animator;
+    public enum Direction { left, right };
+    public Direction direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(direction)
+        {
+            case Direction.left:
+                animator.SetBool("direction", false);
+                break;
+            case Direction.right:
+                animator.SetBool("direction", true);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/StareAtPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StareAtPlayer : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+        else
+        {
+            if (player.transform.position.x > transform.position.x)
+            {
+                GetComponent<NPCIdle>().direction = NPCIdle.Direction.right;
+            }
+            else
+            {
+                GetComponent<NPCIdle>().direction = NPCIdle.Direction.left;
+            }
+        }
+    }
+}
